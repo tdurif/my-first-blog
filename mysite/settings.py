1ebc09653ab54846ded07d4d38d699f12a963a78
tdurif <thomas.durif@gmail.com>
2018-01-04 15:21:06 +0100

    made a correction to mysite/settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-ALLOWED_HOSTS = ['127.0.0.1','alphamo.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','tdurif.pythonanywhere.com']
